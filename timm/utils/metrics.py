76227eaabdee5f0da8b1014ff5ee4fdf145dfe58
EQUASHNZRKUL <justinkae@gmail.com>
2025-01-09 22:41:49 -0500

    Realized I was making changes to the wrong timm

--- timm/utils/metrics.py
+++ timm/utils/metrics.py
@@ -3,6 +3,8 @@
 Hacked together by / Copyright 2020 Ross Wightman
 """
 
+import torch
+import torch.nn.functional as F
 
 class AverageMeter:
     """Computes and stores the average and current value"""
@@ -30,3 +32,27 @@ def accuracy(output, target, topk=(1,)):
     pred = pred.t()
     correct = pred.eq(target.reshape(1, -1).expand_as(pred))
     return [correct[:min(k, maxk)].reshape(-1).float().sum(0) * 100. / batch_size for k in topk]
+
+def pairwise_dist(A, B):
+    na = torch.sum(A**2, dim=1)
+    nb = torch.sum(B**2, dim=1)
+
+    na = na.reshape(-1, 1)
+    nb = nb.reshape(1, -1)
+
+    D = torch.sqrt(torch.maximum(na - 2 * torch.matmul(A, B.T) + nb, torch.tensor(1e-12)))
+    return D
+
+def cross_entropy_nn_pred(enc_x, in_y, learnt_y):
+    """Cross Entropy NN Prediction based on learnt_y."""
+    enc_x_to_learnt_y_dist = pairwise_dist(enc_x, learnt_y)
+    logits = F.softmax(-1. * enc_x_to_learnt_y_dist, dim=1)
+    preds = torch.argmax(logits, dim=1)
+    true_y = torch.argmax(in_y, dim=1)
+    return preds, true_y
+
+def lwal_accuracy(output, target, learnt_y, topk=(1,)):
+    """Computes the 1-accuracy for lwal loss."""
+    pred_y, true_y = cross_entropy_nn_pred(output, target, learnt_y)
+    acc1 = (pred_y == true_y).float().mean() * 100.
+    return acc1, 0.0
