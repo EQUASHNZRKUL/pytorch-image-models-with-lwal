c3e9445ab859506bb9f818d2a5c13ab675f8b5c1
EQUASHNZRKUL <justinkae@gmail.com>
2025-04-28 21:37:21 -0400

    Going back to cossim loss

--- timm/loss/learning_with_adaptive_labels.py
+++ timm/loss/learning_with_adaptive_labels.py
@@ -132,7 +132,7 @@ def cos_repel_loss_z_optimized(z, in_y):
 
     # Compute cosine similarity matrix
     cos_dist = torch.matmul(norm_z, norm_z.T)
-    adj_cos_dist = torch.relu(cos_dist)
+    # adj_cos_dist = torch.relu(cos_dist)
     # p_dist = pairwise_dist(norm_z, norm_z)
     # learnt_y_dist = torch.relu(-torch.matmul(z, z))
 
@@ -144,7 +144,7 @@ def cos_repel_loss_z_optimized(z, in_y):
     class_mask = (true_y_expanded != true_y_expanded.T).float()  # Shape: [batch_size, batch_size]
 
     # Compute the loss: mean of cosine distances for different-class pairs
-    return torch.mean(adj_cos_dist * class_mask)
+    return torch.mean(cos_dist * class_mask)
 
 
 class LearningWithAdaptiveLabels(nn.Module):
