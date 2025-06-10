6f23f0c8a381495961f71cd8b3cee711c37b0c4d
EQUASHNZRKUL <justinkae@gmail.com>
2025-06-09 22:51:16 -0400

    fixing .to bug

--- timm/loss/learning_with_adaptive_labels.py
+++ timm/loss/learning_with_adaptive_labels.py
@@ -179,7 +179,7 @@ def generate_random_orthogonal_vectors(num_classes, latent_dim, device, seed=Non
     random_matrix = np.random.rand(latent_dim, num_classes)
     Q, _ = np.linalg.qr(random_matrix)
     orthogonal = torch.from_numpy(Q.T)
-    orthogonal.to(device)
+    orthogonal = orthogonal.to(device)
     return orthogonal
 
 
